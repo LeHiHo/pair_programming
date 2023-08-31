473aa394044c7aa29a880808ddce7252ceec63cc
sunpaaaa <sunpaaaa@gmail.com>
2023-08-31 12:05:18 +0900

    Add design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-import Item from './item';
+import Item from './Item.js';
 
 function App() {
-  return <Item />;
+  const date = new Date();
+  const today =
+    date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+
+  const dummy = {
+    title: '취업9조대',
+    date: today,
+    content: 'Pair Programming 진행 중!',
+    img: 'https://file.notion.so/f/s/ebf37348-eb25-4f20-b8ea-148d07ed8f93/image.png?id=7e526f8e-f8ff-4ef4-8ff2-d9f718841c0b&table=block&spaceId=3ef8dbd9-414c-4cf5-813d-32ecb943cc67&expirationTimestamp=1693533600000&signature=rFOiNKRuZ6t8XnrXXK8C2UwY3Ob7CseyEUp3x9Wy-o0&downloadName=image.png',
+  };
+  return (
+    <>
+      <Item dummy={dummy} name={'선파'} />
+      <Item dummy={dummy} name={'호진'} />
+      <Item dummy={dummy} name={'연수'} />;
+      <Item dummy={dummy} name={'경규'} />;
+      <Item dummy={dummy} name={'지송'} />;
+    </>
+  );
 }
 
 export default App;
--- src/item.js
+++ src/item.js
@@ -1,33 +1,21 @@
 import React from 'react';
 import { useState } from 'react';
+import './item.css';
 
-const date = new Date();
-
-const today =
-  date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
-
-const dummy = {
-  title: 'title',
-  user: 'sunpa',
-  date: today,
-  content: '어쩌구',
-  img: 'https://file.notion.so/f/s/ebf37348-eb25-4f20-b8ea-148d07ed8f93/image.png?id=7e526f8e-f8ff-4ef4-8ff2-d9f718841c0b&table=block&spaceId=3ef8dbd9-414c-4cf5-813d-32ecb943cc67&expirationTimestamp=1693533600000&signature=rFOiNKRuZ6t8XnrXXK8C2UwY3Ob7CseyEUp3x9Wy-o0&downloadName=image.png',
-};
-
-function Item() {
-  const { title, user, date, content, img } = dummy;
+function Item({ dummy, name }) {
+  const { title, date, content, img } = dummy;
   //   const [item, setItem] = useState(0);
 
   return (
     <>
       <div className='container'>
-        <img src={img}></img>
+        <img className='img' src={img} alt='' />
         <div>
-          <ul>
-            <li>{title}</li>
-            <li>{user}</li>
-            <li>{date}</li>
-            <li>{content}</li>
+          <ul className='info'>
+            <li className='info_title'>{title}</li>
+            <li className='info_user'>{name}</li>
+            <li className='info_date'>{date}</li>
+            <li className='info_content'>{content}</li>
           </ul>
         </div>
       </div>
